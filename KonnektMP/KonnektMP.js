1b535f4946f107ba46f98984eb52d2991c3b2f20
keleko34 <gregory.guidero@gmail.com>
2017-02-20 17:37:01 +0100

    initial commit

---
+++ KonnektMP/KonnektMP.js
@@ -0,0 +1,17 @@
+/* Build */
+/* End Build */
+
+define([],function(){
+
+  function CreateKonnektMP()
+  {
+    function KonnektMP()
+    {
+
+    }
+
+
+    return KonnektMP;
+  }
+  return CreateKonnektMP;
+});
---
+++ index.js
@@ -0,0 +1,1 @@
+module.exports = require( './KonnektMP/Min/KonnektMP.min' );
